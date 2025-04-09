396e005dc04e034d2ec654048c5abb876b6f56eb
ricoi <ricoisdenaam@gmail.com>
2025-04-10 00:44:36 +0200

    Build script created

---
+++ build.bat
@@ -0,0 +1,3 @@
+del /f /q dist\*
+C:\Users\ricoi\PycharmProjects\Discord_tools\.venv\Scripts\python.exe -m build
+C:\Users\ricoi\PycharmProjects\Discord_tools\.venv\Scripts\python.exe -m twine upload .\dist\*
