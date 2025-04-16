da88cb41fe4db5a00995be57d881d57e34796599
ricoi <ricoisdenaam@gmail.com>
2025-04-16 19:03:19 +0200

    Updates to messages.py

--- src/discord_py_utilities/messages.py
+++ src/discord_py_utilities/messages.py
@@ -1,17 +1,58 @@
+import asyncio
 import logging
 
 import discord
 from discord.ext.commands.help import MISSING
 
 from .exceptions import NoChannelException, NoPermissionException
+from .permissions import check_missing_channel_permissions
 
 MAX_LENGTH = 1800
 
 
+async def handle_no_permission(channel: discord.TextChannel | discord.User | discord.Member,
+                               error_mode: str = 'error') :
+	"""
+	Handles no permission errors within the code based on error_modes: error, warn, ignore.
+	Error: Raises NoPermissionException
+	Warn: Logs a warning message and sends a message to the owner of the guild.
+	Ignore: Does nothing.
+	:param channel:
+	:param error_mode:
+	"""
+	match error_mode.lower() :
+		case 'error' :
+			missing_perms = check_missing_channel_permissions(channel,
+			                                                  ['view_channel', 'send_messages', 'embed_links',
+			                                                   'attach_files'])
+			raise NoPermissionException(required_perms=missing_perms, channel=channel)
+		case 'warn' :
+			missing_perms = check_missing_channel_permissions(channel,
+			                                                  ['view_channel', 'send_messages', 'embed_links',
+			                                                   'attach_files'])
+			logging.warning(f"Missing permission to send message to {channel.name}")
+			await channel.guild.owner.send(
+				f"Missing permission to send message to {channel.name}. Check permissions: {', '.join(missing_perms)}", )
+		case 'ignore' :
+			logging.debug(f"Missing permission to send message to {channel.name}. Ignoring.")
+		case _ :
+			logging.debug(f"Missing valid error_mode, defaulting to error.")
+			missing_perms = check_missing_channel_permissions(channel,
+			                                                  ['view_channel', 'send_messages', 'embed_links', 'attach_files'])
+			raise NoPermissionException(required_perms=missing_perms, channel=channel)
+
+
 async def send_message(channel: discord.TextChannel | discord.User | discord.Member, message: str = None, embed=None,
-                       view=None, files=None,
-                       ) -> discord.Message :
-	"""Send a message to a channel, if there is no permission it will send an error message to the owner. Automatically handles messages longer than 2000 characters by splitting them into multiple messages."""
+                       view=None, files=None, error_mode: str = 'error') -> discord.Message :
+	"""Send a message to a channel, if there is no permission it will send an error message to the owner. Automatically handles messages longer than 2000 characters by splitting them into multiple messages.
+	:param channel:
+	:param message:
+	:param embed:
+	:param view:
+	:param files:
+	:param error_mode:
+	:return:
+	"""
 	last_message = None
 	if channel is None :
 		raise NoChannelException
@@ -25,12 +66,12 @@ async def send_message(channel: discord.TextChannel | discord.User | discord.Mem
 		else :
 			return last_message
 	except discord.errors.Forbidden :
-		required_perms = ['view_channel', 'send_messages', 'embed_links', 'attach_files']
-		raise NoPermissionException(required_perms=required_perms, channel=channel)
+		await handle_no_permission(channel, error_mode)
 
 
-async def send_response(interaction: discord.Interaction, response, ephemeral=False, view=MISSING, embed=MISSING) :
+async def send_response(interaction: discord.Interaction, response, ephemeral=False, view=MISSING, embed=MISSING, error_mode = 'error') :
 	"""Sends a response to an interaction. If the interaction is already responded, it will send a followup message.
+	:param error_mode:
 	:param interaction:
 	:param response:
 	:param ephemeral:
@@ -40,14 +81,9 @@ async def send_response(interaction: discord.Interaction, response, ephemeral=Fa
 	"""
 	try :
 
-		return await interaction.response.send_message(response, ephemeral=ephemeral, view=view, embed=embed, )
+		return await interaction.response.send_message(response, ephemeral=ephemeral, view=view, embed=embed)
 	except discord.errors.Forbidden :
-		required_perms = ['view_channel', 'send_messages', 'embed_links', 'attach_files']
-		missing_perms = await check_missing_permissions(interaction.channel, required_perms)
-		logging.error(f"Missing permission to send message to {interaction.channel.name}")
-		await interaction.guild.owner.send(
-			f"Missing permission to send message to {interaction.channel.name}. Check permissions: {', '.join(missing_perms)}", )
-		raise NoPermissionException(missing_permissions=missing_perms)
+		await handle_no_permission(interaction.channel, error_mode=error_mode)
 	except discord.errors.NotFound or discord.InteractionResponded :
 		try :
 			await interaction.followup.send(
@@ -59,8 +95,10 @@ async def send_response(interaction: discord.Interaction, response, ephemeral=Fa
 			)
 		except discord.errors.NotFound :
 			await send_message(interaction.channel, response, view=view, embed=embed)
+
+
 	except Exception :
-		return await interaction.channel.send(response, view=view, embed=embed)
+		return await send_message(interaction.channel ,response, view=view, embed=embed)
 
 
 async def await_message(interaction, message) -> discord.Message | bool :
@@ -83,8 +121,17 @@ async def await_message(interaction, message) -> discord.Message | bool :
 
 
 async def fetch_message_or_none(channel: discord.TextChannel | discord.DMChannel | discord.Thread,
-                                id: int) -> discord.Message | None :
+                                id: int, wait = 0) -> discord.Message | None :
+	"""
+	Fetch a message from a channel. If the message is not found, return None. Wait for a specified amount of time before fetching the message; useful for fetching starting message of threads as they often have delays.
+	:param wait:
+	:return:
+	:param channel:
+	:param id:
+	:return:
+	"""
 	try :
+		await asyncio.sleep(wait)
 		return await channel.fetch_message(id)
 	except discord.errors.NotFound :
 		return None
@@ -92,7 +139,7 @@ async def fetch_message_or_none(channel: discord.TextChannel | discord.DMChannel
 
 async def delete_message(message: discord.Message | discord.Thread) :
 	"""
-
+	Delete a message. If the message is not found, return None. If the message is not found, send a message to the channel.
 	:param message:
 	:return:
 	"""
