a30ef2321d23aee07699810fc3e27d6a0131397e
ricoi <ricoisdenaam@gmail.com>
2025-08-11 21:28:29 +0200

    adding fixes to the invite checker

--- src/discord_py_utilities/invites.py
+++ src/discord_py_utilities/invites.py
@@ -1,28 +1,39 @@
 import logging
+import re
 
 import discord
 from discord.ext import commands
 
-from src.discord_py_utilities.permissions import check_missing_channel_permissions
+from .permissions import check_missing_channel_permissions
 
 
-async def check_guild_invites(self, bot: commands.AutoShardedBot, guild: discord.Guild, current_invite = None) -> str :
+def extract_invite_code(invite_url: str) -> str | None :
+	"""Extracts the invite code from a Discord invite URL."""
+	match = re.search(r"(?:https?://)?(?:www\.)?discord(?:\.gg|app\.com/invite)/([a-zA-Z0-9-]+)", invite_url)
+	return match.group(1) if match else invite_url
+
+
+async def check_guild_invites(bot: commands.AutoShardedBot, guild: discord.Guild, current_invite=None) -> str :
 	invite: None | discord.Invite = None
+
 	if current_invite :
 		try :
-			await bot.fetch_invite(current_invite)
-			return current_invite
+			invite_code = extract_invite_code(current_invite)
+			invite = await bot.fetch_invite(invite_code)
+			return invite.url
 		except discord.HTTPException or discord.NotFound :
 			logging.info(f"{guild.name}'s invite expired, creating a new one.")
+		except ValueError:
+			logging.info(f"{guild.name}'s has weird characters in invite code, {current_invite}")
 	try :
 		for channel in guild.channels :
 			if len(check_missing_channel_permissions(channel, ["view_channel", "create_instant_invite"])) > 0 :
 				continue
 			try :
-				invite = await create_invite(channel, reason=f"Missing Invite/Invalid invite, making a new one.")
+				invite: str = await create_invite(channel, reason=f"Missing Invite/Invalid invite, making a new one.")
 			except discord.NotFound :
 				continue
-			return invite.url
+			return invite
 	except discord.Forbidden :
 		logging.info(f"No permission to create invites in {guild.name}")
 	try :
@@ -39,7 +50,7 @@ async def check_guild_invites(self, bot: commands.AutoShardedBot, guild: discord
 		return "error creating invite"
 
 
-async def create_invite(channel: discord.TextChannel, reason = None, max_age = 604800) -> str :
+async def create_invite(channel: discord.TextChannel, reason=None, max_age=604800) -> str :
 	try :
 		invite = await channel.create_invite(max_age=604800, reason=reason)
 		return invite.url
